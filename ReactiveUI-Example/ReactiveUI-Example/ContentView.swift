cd15d8376487fafefa837c28434d6c4d51f841bf
Dan Berglund <dan@berglund.io>
2020-02-05 12:03:27 +0100

    Initial commit

---
+++ ReactiveUI-Example/ReactiveUI-Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReactiveUI-Example
+//
+//  Created by Dan Berglund on 2020-02-04.
+//  Copyright © 2020 Dan Berglund. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
